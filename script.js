2bd9d5db44ada99d5361285cf6c35e66b74c790e
YasmimRodrigues <yasmimrbm25@gmail.com>
2025-10-22 10:44:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Initial Data
+let currentColor = 'black';
+
+// Events
+document.querySelectorAll('.colorArea .color').forEach(item => {
+    item.addEventListener('click', colorClickEvent);
+});
+
+// Functions
+function colorClickEvent(e) {
+    let color = e.target.getAttribute('data-color');
+    currentColor = color;
+
+    document.querySelector('.color .active').classList.remove('active');
+    e.target.classList.add('active');
+}
